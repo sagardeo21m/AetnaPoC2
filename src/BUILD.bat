4840cf35194bb304e27fad0ca7b492ad46e0f06f
SAGAR DEO <sagardeo@SAGARs-MacBook-Pro.local>
2018-07-04 23:30:04 -0400

    Readding Files

--- src/BUILD.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-javac Main.java
-
-java Main
--- src/HelloBlueOcean.java
+++ src/HelloBlueOcean.java
@@ -1,5 +1,5 @@
 
-public class Test {
+public class HelloBlueOcean {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
